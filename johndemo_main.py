a253cd3ff1db9d25a99792622ffb92643cb4c2c4
jstrickler <jstrickler@gmail.com>
2021-04-16 11:41:45 -0400

    Friday 11:41

--- johndemo_main.py
+++ johndemo_main.py
@@ -5,13 +5,14 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from ui_johndemo import Ui_JohnDemo
 
-class JohnDemoMain(QMainWindow):   
+
+class JohnDemoMain(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
         # Set up the user interface from Designer.
-        self.ui = Ui_JohnDemo()
-        self.ui.setupUi(self)
+        self.ui = Ui_JohnDemo()  # make instance of GUI
+        self.ui.setupUi(self)    # set up GUI
 
         # Connect up menu actions
         # self.ui.actionExit.triggered.connect(self.close)
@@ -19,10 +20,9 @@ class JohnDemoMain(QMainWindow):
         # Connect up buttons.
         # self.ui.BUTTON_NAME.clicked.connect(self._pushed)
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = JohnDemoMain()
     main.show()
     sys.exit(app.exec_())
-
-
